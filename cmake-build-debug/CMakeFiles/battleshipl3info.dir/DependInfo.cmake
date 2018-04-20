67f04d6ad845a6ed16a670c9ec91735f990dc053
Lucas Declercq <lucas.declercq@ineat-conseil.fr>
2018-04-20 11:50:33 +0200

    refacto(folders): create separate folders for the two client versions

---
+++ cmake-build-debug/CMakeFiles/battleshipl3info.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/lucas/Projects/_SCHOOL/battleshipl3info/src/server/main.c" "/home/lucas/Projects/_SCHOOL/battleshipl3info/cmake-build-debug/CMakeFiles/battleshipl3info.dir/src/server/main.c.o"
+  "/home/lucas/Projects/_SCHOOL/battleshipl3info/src/server/server.c" "/home/lucas/Projects/_SCHOOL/battleshipl3info/cmake-build-debug/CMakeFiles/battleshipl3info.dir/src/server/server.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
