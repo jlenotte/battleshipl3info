80afae1fc4af4de2a081182a6a77f621ea0a6186
Lucas Declercq <lucas.declercq@ineat-conseil.fr>
2018-04-20 15:28:48 +0200

    feat(init): add boat and player initialization

--- src/shared/message.h
+++ src/shared/message.h
@@ -1,55 +1,63 @@
-typedef struct Player {
-    int id;
-    char nickname[255];
-    s_boat boat1;
-    s_boat boat2;
-} s_player;
+// 1 : Message de coup | [ Strike ] | ( on reçoit les coordonnées )
+// 8 :         | Fin de partie          | [ char nickname[255]; ] ( Le nickname du vainqueur )
+// 7 :         | Bilan tour             | [ SucccessfullStrike strikes[n], Player eliminatedPlayers[n] ] (une liste des tirs réussis + une liste des joueurs éliminés)
+// 6 :         | Notification tir       | [ char nickname[255];  ( Le nickname du joueur qui a joué)
+// 5 :         | Message échec tir      | [ void ]
+// 4 :         | Message succès tir     | [ char nickname[255] ] ( Le nickname du joueur touché )
+// 3 :         | Début de tour          | [ void ] ( Notification du début du tour )
+// 2 :         | Début de partie        | [ void ] ( Tous les joueurs sont prêts )
+// 1 :         | Connection établie     | [ Player player ] ( Confirme la connexion, envoie de la position des bateaux )
+typedef struct Boat Boat;
+typedef struct Strike Strike;
+typedef struct Player Player;
+typedef struct SuccessfulStrike SuccessfulStrike;
+typedef struct OutgoingMessage OutgoingMessage;
+typedef struct IncomingMessage IncomingMessage;
 
-typedef struct Boat {
+struct Boat {
     int x;
     int y;
     int isAlive;
-} s_boat;
+};
 
-typedef struct Strike {
+struct Player {
+    int id;
+    char nickname[255];
+    Boat boat1;
+    Boat boat2;
+};
+
+struct Strike {
     int x;
     int y;
-    s_player fromPlayer;
-} s_strike;
+    Player fromPlayer;
+};
 
-typedef struct SuccessfulStrike {
+struct SuccessfulStrike {
     char nickPlayerFrom[255];
     char nickPlayerTo[255];
-    s_boat boat;
-} s_successfulStrike;
+    Boat boat;
+};
 
 // le type message définit ce qu'il faut lire dans la struct envoyé
 
 // typeMessage | description            | [ type ] |
-// 0 :         | En attente connection   | [ char genericMessage[255]; ] ( "En attente de connection n joueurs" )
-// 1 :         | Connection établie     | [ Player player ] ( Confirme la connexion, envoie de la position des bateaux )
-// 2 :         | Début de partie        | [ void ] ( Tous les joueurs sont prêts )
-// 3 :         | Début de tour          | [ void ] ( Notification du début du tour )
-// 4 :         | Message succès tir     | [ char nickname[255] ] ( Le nickname du joueur touché )
-// 5 :         | Message échec tir      | [ void ]
-// 6 :         | Notification tir       | [ char nickname[255];  ( Le nickname du joueur qui a joué)
-// 7 :         | Bilan tour             | [ SucccessfullStrike strikes[n], Player eliminatedPlayers[n] ] (une liste des tirs réussis + une liste des joueurs éliminés)
-// 8 :         | Fin de partie          | [ char nickname[255]; ] ( Le nickname du vainqueur )
 
-typedef struct OutgoingMessage {
+struct OutgoingMessage {
     int typeMessage;
     char genericMessage[255];
-    s_player player;
+    Player player;
     char nickname[255];
-    s_successfulStrike strikes[15];
-    s_player eliminatedPlayers[15];
-} s_outgoingMessage;
+    SuccessfulStrike strikes[15];
+    Player eliminatedPlayers[15];
+};
 
 // 0 : Message nickname | [ char nickname[255] ] | ( le pseudo du joueur )
-// 1 : Message de coup | [ Strike ] | ( on reçoit les coordonnées )
 
-typedef struct IncomingMessage {
+struct IncomingMessage {
     int typeMessage;
     char nickname[255];
-    s_strike strike;
-} s_incomingMessage;
+    Strike strike;
+};
+
+// 0 :         | En attente connection   | [ char genericMessage[255]; ] ( "En attente de connection n joueurs" )
